e6e8c1fe48ac1916693f3901a171c00ae797f87d
Oddstream <oddstream@googlemail.com>
2021-04-01 15:55:29 +0100

    go:embed Acme font

--- maze/acmefonts.go
+++ maze/acmefonts.go
@@ -1,13 +1,15 @@
-// Copyright ©️ 2021 oddstream.games
-
 package maze
 
 import (
+	_ "embed" // go:embed only allowed in Go files that import "embed"
 	"log"
 
 	"github.com/golang/freetype/truetype"
 	"golang.org/x/image/font"
 )
+
+//go:embed assets/Acme-Regular.ttf
+var acmeFontBytes []byte
 
 // AcmeFonts contains references to small, normal, large and huge Acme fonts
 type AcmeFonts struct {
@@ -20,17 +22,7 @@ type AcmeFonts struct {
 // NewAcmeFonts loads some fonts and returns a pointer to an object referencing them
 func NewAcmeFonts() *AcmeFonts {
 
-	// path, err := filepath.Abs("/home/gilbert/Tetra/assets/Acme-Regular.ttf")
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// golang gotcha use full path so "go test" can find asset
-	// bytes, err := ioutil.ReadFile("/home/gilbert/Tetra/assets/Acme-Regular.ttf")
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// https://pkg.go.dev/golang.org/x/image@v0.0.0-20201208152932-35266b937fa6/font
-	tt, err := truetype.Parse(Acme_ttf)
+	tt, err := truetype.Parse(acmeFontBytes)
 	if err != nil {
 		log.Fatal(err)
 	}
